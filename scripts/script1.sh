b8b2a9f8030939df2ec7b3eba62eb71eef470458
staloosh <bogdan.astalus@gmail.com>
2022-09-21 19:40:19 +0300

    Add bash script for log parsing

---
+++ scripts/script1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+## This script is used to filter out journalctl logs for the k3s service, searching and printing for the error messages
+
+## Variable definition
+LOG_FILE="journal.log"
+OUTPUT="output.txt"
+
+## Define function to parse logs
+parser() {
+    echo " -------------------------------------- "
+    echo -e "\e[91m You chose $word, let's see the output \e[0m"
+    echo " -------------------------------------- "
+    grep  $word ${LOG_FILE} > ${OUTPUT} 
+    if [ $? -eq 0 ]; then
+        awk -F 'msg=' '{ print $2 }' ${OUTPUT} ; awk NF >${OUTPUT} 
+        cat $OUTPUT
+        exit 0
+    else
+        echo "error processing request"
+        exit 1
+    fi
+}
+
+## Multi-choice menu
+PS3='Choose what keyword you are looking for in the journalctl log: '
+keywords=("error" "failed" "quit")
+select word in "${keywords[@]}"; do
+    case $word in
+    "error")
+        parser
+        ;;
+    "failed")
+        parser
+        ;;
+    "quit")
+        echo "I see you don't want to filter logs today, goodbye"
+        exit 0
+        ;;
+    *) echo "invalid option chosen, please enter the digit not the word" ;;
+    esac
+done
