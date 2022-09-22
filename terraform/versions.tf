7ba23dca4e55b7d60e430226238b85b1e703e630
staloosh <bogdan.astalus@gmail.com>
2022-09-22 12:02:53 +0300

    Add terraform cloud remote backend

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,5 +1,12 @@
 terraform {
   required_version = ">= 1.0"
+  backend "remote" {
+    organization = "staloosh"
+
+    workspaces {
+      name = "personal-project"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
